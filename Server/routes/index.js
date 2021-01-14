00cbf36b433f65bad59409d327304758da004bd5
Nguyen Dong Hung <ndh1379@gmail.com>
2021-01-14 21:04:42 +0700

    config route / for received message if server is alive

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.status(200).send({ message: "server is running!" });
 });
 
 module.exports = router;
