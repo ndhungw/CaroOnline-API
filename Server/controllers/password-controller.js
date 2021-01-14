9c2cb8a4714442ffff8bd9dc11ecb901c12b72f1
Nguyen Dong Hung <ndh1379@gmail.com>
2021-01-14 09:37:25 +0700

    change URL sent in mail to activate & reset password

--- Server/controllers/password-controller.js
+++ Server/controllers/password-controller.js
@@ -37,8 +37,9 @@ passwordController.recover = async (req, res) => {
       );
       const link =
         "http://" +
-        req.headers.host +
-        "/api/auth/reset/" /* change this part to a link of front end which will render a view for typing reset password */ +
+        // req.headers.host +
+        process.env.FRONTEND_DOMAIN +
+        "/reset-password/" /* change this part to a link of front end which will render a view for typing reset password */ +
         userAfterSave.resetPasswordToken;
       const mailOptions = {
         to: user.email,
